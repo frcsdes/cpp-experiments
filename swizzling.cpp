c77936aaab1d8ab78278110119236981335c2cf4
François Desrichard <git@desrichard.fr>
2020-03-11 13:41:21 +0100

    Fix stream output in swizzling

--- swizzling.cpp
+++ swizzling.cpp
@@ -13,32 +13,31 @@ constexpr Mask<C...> operator""_m() {
 
 
 template<auto N, class T = float>
-struct Vector : std::array<T, N> {};
+struct Vec : std::array<T, N> {};
 
 template<auto N, class T, char... C>
-constexpr Vector<sizeof...(C), T>
-operator|(Vector<N, T> const& vector, Mask<C...>) {
+constexpr Vec<sizeof...(C), T> operator|(Vec<N, T> const& vector, Mask<C...>) {
     return {std::get<static_cast<std::size_t>(C - 48)>(vector)...};
 }
 
 template<auto N, class T>
-std::ostream& operator<<(std::ostream& os, Vector<N, T> const& vector) {
-    std::apply([](auto... c) {
-        ((std::cout << c << ' '), ...) << '\n';
-    }, static_cast<std::array<T, N>>(vector));
+std::ostream& operator<<(std::ostream& os, Vec<N, T> const& vector) {
+    std::apply([&os](auto... coefficient) {
+        ((os << coefficient << ' '), ...) << '\n';
+    }, static_cast<std::array<T, N> const&>(vector));
     return os;
 }
 
 
 int main() {
-    // Swizzling by using a sequence of indices
+    // Swizzling by using a mask of indices
     // Example: vec.zyx becomes vec | 210_m
 
-    constexpr Vector<4> sequence{10, 11, 12, 13};
+    constexpr Vec<4> sequence{10, 11, 12, 13};
     constexpr auto reverse = sequence | 3210_m;
     std::cout << sequence << reverse;
 
-    constexpr Vector<2> bernoulli{-1, 1};
+    constexpr Vec<2> bernoulli{-1, 1};
     constexpr auto samples = bernoulli | 01110100_m;
     std::cout << samples;
 
